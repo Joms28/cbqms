a255bef5169918857528b46b32db2582debfbf1b
Joseph Magat <xephyre619@gmail.com>
2021-12-10 18:43:28 +0800

    Update Main.php

--- application/models/Main.php
+++ application/models/Main.php
@@ -496,7 +496,7 @@ class Main extends CI_Model {
     return $this->query->row_array();
   }
 
-  public function check_if_has_agent_id($transaction_id){    
+  public function check_if_has_agent_id($transaction_id){
     $query = $this->db->where('id',$transaction_id)->get('transactions')->row_array();
     if($query['agent_id'] == 0){
       return false;
