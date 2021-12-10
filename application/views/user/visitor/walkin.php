475d529d75852ead00b1a7122c0591bfd04f17a4
Jhon Israel Morales <jmorales@cloudpanda.ph>
2021-12-10 23:05:12 +0800

    update

--- application/views/user/visitor/walkin.php
+++ application/views/user/visitor/walkin.php
@@ -38,13 +38,13 @@
     					<?php endif; ?>
 
     						<div class="form-group">
-    							<label>Last Name<label style="color:red">*</label></label>
+    							<label>Last Name</label>
     							<input type="text" class="form-control" name="lname" value="" >
     							<?php echo form_error('lname', '<small><span style="color:red;font-size: small">', '</strong></small><br>'); ?>
     						</div>
 
                 <div class="form-group">
-    							<label>First Name<label style="color:red">*</label></label>
+    							<label>First Name</label>
     							<input type="text" class="form-control" name="fname" >
     							<?php echo form_error('fname', '<small><span style="color:red;font-size: small">', '</strong></small><br>'); ?>
     						</div>
@@ -52,7 +52,7 @@
                 <input type="hidden" name="email" value="default-email@g.com">
 
                 <div class="form-group">
-    							<label>Mobile Number<label style="color:red">*</label></label>
+    							<label>Mobile Number</label>
     							<input type="text" class="form-control" name="mobile" >
     							<?php echo form_error('mobile', '<small><span style="color:red;font-size: small">', '</strong></small><br>'); ?>
     						</div>
